a25447f5deda59307c3c15077519bca453ec363e
Denis Ignatenko <d.i.ignatenko@gmail.com>
2022-08-24 20:11:23 +0300

    Спринт 3. Финальное задание на проверку

---
+++ yatube/posts/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=20, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        related_name='group',
+        blank=True, null=True
+    )
+
+
+# class Event(models.Model):
+#     name = models.CharField(max_length=200)
+#     start_at = models.DateTimeField()
+#     description = models.TextField(max_length=200)
+#     contact = models.EmailField()
+#     author = models.ForeignKey(
+#         User,
+#         on_delete=models.CASCADE,
+#         related_name='events'
+#     )
+#     location = models.CharField(max_length=400)
