7ccaf6fcee689b4b45b0579cb569fadd507d535b
Denis Ignatenko <d.i.ignatenko@gmail.com>
2022-09-01 00:06:38 +0300

    Спринт 3. Финальный проект. Правки по комментариям ревьера №2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -17,9 +17,7 @@ def index(request):
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-
     posts = group.posts.all()[:posts_per_page]
-    # posts = Post.objects.filter(group=group)[:posts_per_page]
     context = {
         'group': group,
         'posts': posts
